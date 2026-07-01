86f25bccc45e605da681f008e6281d2086498659
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2026-07-01 11:03:42 -0300

    Criando ProductDetailScreen.tsx além de colocar produtos dentro do array que criamos no commit passado, criando constante de estilos antes da estrutura da tela de detalhes do produto

--- screens/ProductDetailScreen.tsx
+++ screens/ProductDetailScreen.tsx
@@ -18,3 +18,81 @@ import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { RootStackParamList } from "./HomeScreen";
 
 import { getProdutoById } from "../data/produtos";
+
+type Props = NativeStackScreenProps<RootStackParamList, 'ProductDetail'>;
+
+export default function ProductDetailScreen({ navigation, route }: Props) {
+    const { productId } = route.params;
+    const produto = getProdutoById(productId);
+    const [quantidade, setQuantidade] = useState(1);
+    const insets = useSafeAreaInsets();
+
+    if (!produto) {
+        return (
+            <SafeAreaView>
+
+            </SafeAreaView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    scroll: {
+    },
+    scrollContent: {
+    },
+    headerButton: {
+    },
+    headerButtonLeft: {
+    },
+    headerButtonRight: {
+    },
+    productImage: {
+    },
+    brandRow: {
+    },
+    brandLogo: {
+    },
+    brandName: {
+    },
+    productName: {
+    },
+    priceRow: {
+    },
+    price: {
+    },
+    quantitySelector: {
+    },
+    quantityButtonMinus: {
+    },
+    quantityButtonPlus: {
+    },
+    quantityText: {
+    },
+    sectionTitle: {
+    },
+    aboutText: {
+    },
+    ingredientsHeader: {
+    },
+    ingredientRow: {
+    },
+    bullet: {
+    },
+    ingredientText: {
+    },
+    bottomSpacer: {
+    },
+    footer: {
+    },
+    addButton: {
+    },
+    addButtonText: {
+    },
+    errorText:{
+    },
+    backLink: {
+    },
+});
